9ad0eae150ae3d53d7f45017a096fb5658715d72
hirakawa-saki <ansu11922964@gmail.com>
2023-12-12 22:54:49 +0900

    1212提出

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,73 +1,70 @@
 "use strict";
 
 $(function () {
-  let pageNumber = 1;
-  let previousSearchWord = "";
+  let pageNum = 0;
+  let prevWord = "";
 
   $(".search-btn").on("click", function () {
-    const searchWord = $("#search-input").val();
-    if (searchWord !== previousSearchWord) {//サーチワードと前回のワードが異なる場合、ページを１にリセットし、リストをemptyで無にする。
-      pageNumber = 1;
+    pageNum = pageNum + 1
+    var searchWord = $("#search-input").val();
+    if (searchWord !== prevWord) { //サーチワードと前回のワードが異なる場合、ページを１にリセットし、リストをemptyで無にする。
+      pageNum = 1;
       $(".lists").empty();
-      previousSearchWord = searchWord;
+      $(".message").remove();
+      prevWord = searchWord;
     } else {
-      pageNumber++;//同じ検索ワードが使用されている場合はページ数を増やす。
+      pageNum++;//同じ検索ワードが使用されている場合はページ数を増やす。
     }
 
-    const apiSettings = {
-      "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}"&format=json&p=${pageNumber}&count=20`,
-      "method": "GET",
-    };
 
-    $.ajax(apiSettings)
-      .done(function (response) {
-        const success = response['@graph'];
-        displayResults(success);
-      })//リクエストが成功すると変数サクセスを表示させる。
+  $.ajax({
+    url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageNum}&count=20`,
+    method: "GET",
+    dataType:'jsonp',
+
+
+  }).done(function (response) {
+      if (response.count > 0) {
+        var list = "";
+        $(".comment").remove();
+        for (var i = 0; i < response.Items.length; i++) {
+          list += `<li class=lists__item>` +
+            `<div class=lists__item__inner>` +
+            `<a href=${response.Items[i].Item.itemUrl} class=lists__item__link target=_blank>` +
+            `<img src=${response.Items[i].Item.largeImageUrl} class=lists__item__img alt>` +
+            `<p class=lists__item__detail>作品名：　${response.Items[i].Item.title}</p>` +
+            `<p class=lists__item__detail>作者　：　${response.Items[i].Item.author}</p>` +
+            `<p class=lists__item__detail>出版社：　${response.Items[i].Item.publisherName}</p>` + `</a>` +
+            `</div>` + `</li>`;
+        };
+        $(".lists").prepend(list);
+      } else {
+        $(".comment").remove();
+        $(".lists").before("<div class='comment'></div>");
+        $(".comment").html("<p class='message'>検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</p>");
+      };
+    })
       .fail(function (err) {
         $(".message").remove();
         displayError(err);
-      });//リクエストが失敗すると変数エラーを表示させる。
+      });
   });
-
-  $(".reset-btn").on("click", function () {
-    pageNumber = 1;
-    previousSearchWord = "";
-    $(".lists").empty();
-    $("#search-input").val("");
-    $(".message").remove();
-  });//リセットボタンを押すとページ数を１に、検索結果と条件をリセット
-
-  function displayResults(success) {
-    if (success && success.length > 0) {//１つ以上のsuccessがある時に実行
-      const extractedData = success.map(item => ({//extractedDateという関数にsuccessからデータを格納。
-        title: item.title || "タイトル不明",
-        author: item["dc:creator"] || "著者不明",
-        publisher: item["dc:publisher"] ? item["dc:publisher"][0] : "出版社不明",
-        link: item.link["@id"]
-      }));
-      if (extractedData.length > 0) {
-        $.each(extractedData, function (i,item) {
-          const listItem = `<li class="lists-item">
-            <div class="list-inner">
-              <p>タイトル：${item.title}</p>
-              <p>著者：${item.author}</p>
-              <p>出版社：${item.publisher}</p>
-              <a href="${item.link}" target="_blank">書籍情報</a>
-            </div>
-          </li>`;
-          $(".lists").prepend(listItem);
-        });
-      } else {//検索結果が１つもなかった場合メッセージを表示
-        $(".lists").before('<div class="message">検索結果が見つかりませんでした。</div>');
-      }
-    }
-  }
-
-  function displayError(err) {//APIがうまく送信されなかったときのエラー
-    $(".lists").before('<div class="message">エラーが発生しました。<br>再度更新してください。</div>');
-  }
 });
+
+
+$(".reset-btn").on("click", function () {
+  pageNum = 0;
+  prevWord = "";
+  $(".lists").empty();
+  $("#search-input").val("");
+  $(".message").remove();
+});//リセットボタンを押すとページ数を１に、検索結果と条件をリセット
+
+
+function displayError(err) {
+  $(".lists").before('<div class="message">エラーが発生しました。<br>再度更新してください。</div>');
+}//APIがうまく送信されなかったときのエラー
+
 
 //  変数settingsに設定情報などを格納
 // const settings = {
